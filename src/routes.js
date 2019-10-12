c199f38bb2ab476cced966ddddeda55ff1b47473
djagya <djagya@fastmail.com>
2019-10-12 17:39:48 +0200

    pairing validation

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,14 @@ const isDev = process.env.NODE_ENV === 'development';
 const OTP_EXPIRATION = 5 * 60;
 const OTP_ATTEMPTS_LIMIT = 3;
 
+const PAIRING_EXPIRATION = 2 * 60;
+
+const isCodeValid = (saved, code, expiration) => saved && saved.code
+  // Code matches.
+  && saved.code === code
+  // Not expired.
+  && (!expiration || (Date.now() - new Date(saved.createdAt).getTime()) / 1000 < expiration);
+
 module.exports = function (app, db, client) {
   // Not sure what is supposed to be an authentication method of user,
   // so I use a phone number that is naturally must be available for each request.
@@ -15,30 +23,35 @@ module.exports = function (app, db, client) {
   const badRequest = (res, msg) => res.status(400).json({ error: msg });
   const log = (...args) => isDev && console.log(...args);
 
+  const logAttempt = async (phone, type, code) => {
+    await users.updateOne({ phone }, { $push: { attempts: { type, code, createdAt: new Date() } } });
+    log(`Logged an attempt of ${type} validation: code=${code}, user=${phone}`);
+  };
+
   /**
    * OTP validation.
+   * Required body params:
+   * - phone: the user phone to identify
+   * - code: OTP code to validate
    */
   app.post('/user/otp', async (req, res) => {
-    const { phone, otp } = req.body;
-
-    const isCodeValid = (savedOTP, code) => savedOTP && savedOTP.code
-      // Code matches.
-      && savedOTP.code === code
-      // Not expired.
-      && (Date.now() - new Date(savedOTP.createdAt).getTime()) / 1000 < OTP_EXPIRATION;
+    const { phone, code } = req.body;
 
     if (!phone) {
       return badRequest(res, 'No phone number provided');
     }
-    if (!otp) {
-      return badRequest(res, 'No OPT provided');
+    if (!code) {
+      return badRequest(res, 'No OTP provided');
     }
     const user = await users.findOne({ phone });
     if (!user) {
       return userNotFound(res, phone);
     }
     log('Found user', { user });
-    if (user.verified) {
+    if (user.requiresReset) {
+      return badRequest(res, 'You must contact support to start over');
+    }
+    if (user.verified || user.paired) {
       return badRequest(res, 'You are already verified');
     }
     if (!user.otp) {
@@ -56,14 +69,18 @@ module.exports = function (app, db, client) {
     try {
       await session.withTransaction(async () => {
         // Log an attempt.
-        // await users.updateOne({ phone }, { $push: { attempts: { type: 'otp', code: otp, createdAt: new Date() } } });
-        log(`Logged an attempt of OTP validation: code=${otp}, user=${phone}`);
+        await logAttempt(phone, 'otp', code);
         attemptsCount += 1;
 
         // Mark as verified if valid.
-        if (isCodeValid(user.otp, otp)) {
+        if (isCodeValid(user.otp, code, OTP_EXPIRATION)) {
           isValid = true;
           await users.updateOne({ phone }, { $set: { verified: true } });
+          return;
+        }
+        // If invalid and limit reached, mark user as requiring reset.
+        if (attemptsCount >= OTP_ATTEMPTS_LIMIT) {
+          await users.updateOne({ phone }, { $set: { requiresReset: true } });
         }
       });
     } catch (err) {
@@ -77,14 +94,74 @@ module.exports = function (app, db, client) {
         : badRequest(res, `Invalid OTP code. Remaining attempts ${OTP_ATTEMPTS_LIMIT - attemptsCount}`);
     }
 
-    return res.json({ message: 'Successful validation' });
+    return res.json({ message: 'Successfully validated' });
   });
 
   /**
    * Pairing code validation.
+   * Required body params:
+   * - phone: the user phone to identify
+   * - code: pairing code to validate
    */
-  app.post('/user/pair', (req, res) => {
-    res.send('pairing validation');
+  app.post('/user/pair', async (req, res) => {
+    const { phone, code } = req.body;
+
+    if (!phone) {
+      return badRequest(res, 'No phone number provided');
+    }
+    if (!code) {
+      return badRequest(res, 'No pairing code provided');
+    }
+    const user = await users.findOne({ phone });
+    if (!user) {
+      return userNotFound(res, phone);
+    }
+    log('Found user', { user });
+    if (user.requiresReset) {
+      return badRequest(res, 'You must contact support to start over');
+    }
+    if (!user.verified) {
+      return badRequest(res, 'You are not verified yet. Start with OTP verification.');
+    }
+    if (user.paired) {
+      return badRequest(res, 'You are already paired with the scooter');
+    }
+    if (!user.pairing) {
+      return badRequest(res, 'The pairing code is not generated. Start with OTP verification.');
+    }
+
+    // Log an attempt and validate in a transaction.
+    let errorMessage;
+    const session = client.startSession();
+    try {
+      await session.withTransaction(async () => {
+        // Log an attempt.
+        await logAttempt(phone, 'pairing', code);
+
+        // If code is already expired, lock the user.
+        if ((Date.now() - new Date(user.pairing.createdAt).getTime()) / 1000 > PAIRING_EXPIRATION) {
+          await users.updateOne({ phone }, { $set: { requiresReset: true } });
+          errorMessage = 'Pairing code expired. Contact the support to start over.';
+          return;
+        }
+
+        // Validate and pair.
+        if (isCodeValid(user.pairing, code)) {
+          await users.updateOne({ phone }, { $set: { paired: true } });
+        } else {
+          errorMessage = 'Invalid pairing code';
+        }
+      });
+    } catch (err) {
+      log('Failed transaction', err.message);
+      return res.status(500).json({ error: 'Server error' });
+    }
+
+    if (errorMessage) {
+      return badRequest(res, errorMessage);
+    }
+
+    return res.json({ message: 'Successfully paired' });
   });
 
   /**
